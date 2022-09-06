93bec48fc69c9f4b5bb056167ad228dc869147c7
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-06 00:29:23 -0300

    Inserindo implemtações de lógica feitas no escopo do projeto

--- src/main/java/com/example/relacionamentoEntreTabelas/model/ItemPedidoModel.java
+++ src/main/java/com/example/relacionamentoEntreTabelas/model/ItemPedidoModel.java
@@ -1,5 +1,6 @@
 package com.example.relacionamentoEntreTabelas.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
